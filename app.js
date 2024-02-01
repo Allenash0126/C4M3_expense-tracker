7f4d43fb80e93cd8be6fe6376337cc636077446e
allenash0126 <allen64031@hotmail.com>
2024-02-01 17:19:29 +0800

    build basic routes and  structure for 3 page: edit, index, new.

--- app.js
+++ app.js
@@ -7,17 +7,41 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended : true }))
 
 app.get('/', (req, res) => {
+  res.redirect('/track')
+})
+
+app.get('/track', (req, res) => {
   res.render('index')
 })
 
-app.get('/new', (req, res) => {
+app.get('/track/new', (req, res) => {
   res.render('new')
 })
 
-app.get('/edit', (req, res) => {
+app.post('/track/new', (req, res) => {
+  const info = req.body
+  const name = info.name
+  console.log(info)
+  res.redirect('/track')
+})
+
+app.get('/track/edit', (req, res) => {
+  // 待新增id路由
   res.render('edit')
+})
+
+app.post('/track/edit', (req, res) => {
+  // 待新增id路由
+  const info = req.body
+  console.log(info)
+  res.redirect('/track')
+})
+
+app.get('/track/delete', (req,res) => {
+  res.send('It has been deleted')
 })
 
 app.get('/logout', (req, res) => {
