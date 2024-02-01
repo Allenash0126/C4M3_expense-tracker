92142fd7a512fc333ecb7b5ef8f3462d9f12bc4f
allenash0126 <allen64031@hotmail.com>
2024-02-01 15:50:20 +0800

    initialize layout

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('.hbs', engine({extname: '.hbs'}))
+app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+app.get('/edit', (req, res) => {
+  res.render('edit')
+})
+
+app.get('/logout', (req, res) => {
+  res.send('Logout bye~')
 })
 
 app.listen(port, () => {
