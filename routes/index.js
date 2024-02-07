35a85e9007dc530967d0d59bc26c6cafd10f18b0
allenash0126 <allen64031@hotmail.com>
2024-02-07 11:20:20 +0800

    add Register function

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const tracks = require('./tracks')
+const users = require('./users')
 router.use('/tracks', tracks) 
 //除了引用tracks.js，也將重複的路由/tracks內建，可移除tracks.js內的/tracks路由
+router.use('/users', users) 
 
 router.get('/', (req, res) => {
   res.redirect('/tracks')
---
+++ routes/users.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+const User = db.User
+
+router.post('/', (req, res, next) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  if (password !== confirmPassword) {
+    req.flash('fail', '密碼與驗證密碼不同:(')
+    res.redirect('/register')
+  }
+
+  
+  return User.findAll({
+    attributes: ['name', 'email', 'password'],
+    raw: true
+  })
+    .then((users) => {
+      const isEmailExist = users.find((user) => email === user.email)
+      if(isEmailExist) {
+        req.flash('fail', '已有帳戶，請直接登入:)')
+        return res.redirect('/login')
+      }
+      return User.create({ name, email, password, confirmPassword })
+        .then(() => {
+          req.flash('success', '註冊成功！ 請登入:)')
+          res.redirect('/login')
+        })
+        .catch((error) => {
+          error.errorMessage = '註冊失敗:('
+          next(error)          
+        })
+    })  
+})
+
+module.exports = router
